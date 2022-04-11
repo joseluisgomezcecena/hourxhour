48db18ce388d85246949d969498e21b08e36c9f4
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-04-11 13:53:01 -0700

    Added SMTP Options for annonymus users on config/email.php

--- application/controllers/Machines.php
+++ application/controllers/Machines.php
@@ -14,6 +14,58 @@ class Machines extends CI_Controller{
 		$this->load->view('templates/header');
 		$this->load->view('machines/index', $data);
 		$this->load->view('templates/footer');
+
+		/*
+		$this->load->library('email');
+
+		$subject = 'This is a test';
+		$message = '
+			<p>This message has been sent for testing purposes.</p>';
+
+			// Get full html:
+					$body = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+			<html xmlns="http://www.w3.org/1999/xhtml">
+			<head>
+				<meta http-equiv="Content-Type" content="text/html; charset=' . strtolower(config_item('charset')) . '" />
+				<title>' . html_escape($subject) . '</title>
+				<style type="text/css">
+					body {
+						font-family: Arial, Verdana, Helvetica, sans-serif;
+						font-size: 16px;
+					}
+				</style>
+			</head>
+			<body>
+			' . $message . '
+			</body>
+			</html>';
+			// Also, for getting full html you may use the following internal method:
+			//$body = $this->email->full_html($subject, $message);
+
+			// Attaching the logo first.
+
+			// The last additional parameter is set to true in order
+			// the image (logo) to appear inline, within the message text:
+					//$this->email->attach($file_logo, 'inline', null, '', true);
+					//$cid_logo = $this->email->get_attachment_cid($file_logo);
+					//$body = str_replace('cid:logo_src', 'cid:'.$cid_logo, $body);
+			// End attaching the logo.
+
+			$result = $this->email
+				->from('jgomez@martechmedical.com')
+				->reply_to('jgomez@martechmedical.com')    // Optional, an account where a human being reads.
+				->to('jgomez@martechmedical.com')
+				->subject($subject)
+				->message($body)
+				->send();
+
+			var_dump($result);
+			echo '<br />';
+			echo $this->email->print_debugger();
+
+			exit;
+		*/
+
 	}
 
 
