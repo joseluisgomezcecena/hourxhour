7d69e9a309ab1ecb1821ecf92f17c7bb1476b07e
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-04-11 14:37:52 -0700

    Added Mail_Alerts Controller with test mail function.

---
+++ application/controllers/Mail_Alerts.php
@@ -0,0 +1,34 @@
+<?php
+
+class Mail_Alerts extends CI_Controller
+{
+
+
+	public function test_mail()
+	{
+		$this->load->library('email');
+
+		$subject = 'This is a test';
+		$message = '<p>This message has been sent for testing purposes.</p>';
+
+		$body = $this->email->full_html($subject, $message);
+
+
+		$result = $this->email
+			->from('jgomez@martechmedical.com')
+			->reply_to('jgomez@martechmedical.com')    // Optional, an account where a human being reads.
+			->to('jgomez@martechmedical.com')
+			->subject($subject)
+			->message($body)
+			->send();
+
+		var_dump($result);
+		echo '<br />';
+		echo $this->email->print_debugger();
+
+		exit;
+
+	}
+
+
+}
