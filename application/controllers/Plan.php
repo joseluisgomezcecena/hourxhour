93b706a3243196a4bc5576fef2f788b834f546fc
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-04-21 16:42:37 -0700

    Added mailer

--- application/controllers/Plan.php
+++ application/controllers/Plan.php
@@ -86,6 +86,8 @@ class Plan extends CI_Controller
         } else {
             $this->db->where('plan_id', $plan->plan_id);
             $this->db->update('production_plans', $data);
+
+			$this->sendMail($plan->plan_id);
         }
 
 
@@ -228,4 +230,87 @@ class Plan extends CI_Controller
         $this->load->view('pages/plan/measuring_point', $data);
         $this->load->view('templates/footer');
     }
+
+
+
+
+
+
+
+
+	public function sendMail($plan_id)
+	{
+		$id = $plan_id;
+
+		$recipients = array();
+		//getting data for email.
+		$query = $this->db->query('SELECT * FROM production_plans 
+    LEFT JOIN assets ON production_plans.asset_id = assets.asset_id 
+    LEFT JOIN sites ON assets.site_id = sites.site_id 
+    LEFT JOIN plants ON sites.plant_id = plants.plant_id 
+    LEFT JOIN mail_list ON mail_list.plant_id = plants.plant_id 
+	WHERE production_plans.plan_id = $id');
+
+		$result = $query->result_array();
+
+		foreach ($result as $item)
+		{
+			$recipients[] =  $item['email'];
+		}
+
+		echo $emails = implode(',', $recipients);
+
+
+
+
+		$this->load->library('email');
+
+		$subject = 'Plan has been updated!';
+		$message = '
+			<p>Production plan has been updated.</p>';
+
+		// Get full html:
+		$body = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+			<html xmlns="http://www.w3.org/1999/xhtml">
+			<head>
+				<meta http-equiv="Content-Type" content="text/html; charset=' . strtolower(config_item('charset')) . '" />
+				<title>' . html_escape($subject) . '</title>
+				<style type="text/css">
+					body {
+						font-family: Arial, Verdana, Helvetica, sans-serif;
+						font-size: 16px;
+					}
+				</style>
+			</head>
+			<body>
+			' . $message . '
+			</body>
+			</html>';
+		// Also, for getting full html you may use the following internal method:
+		//$body = $this->email->full_html($subject, $message);
+
+
+
+
+		$result = $this->email
+			->from('jgomez@martechmedical.com')
+			->reply_to('jgomez@martechmedical.com')    // Optional, an account where a human being reads.
+			//->to('jgomez@martechmedical.com')
+			->to("$emails")
+			->subject($subject)
+			->message($body)
+			->send();
+
+		var_dump($result);
+		echo '<br />';
+		echo $this->email->print_debugger();
+		exit;
+	}
+
+
+
+
+
+
+
 }
