4f0e5953b41a44bc3d2a57b1056506038c95315e
Magui Campos <88505411+MaguiCampos@users.noreply.github.com>
2022-05-19 08:20:28 -0700

    test production

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -17,13 +17,13 @@
 <!--<script src="<?php echo  base_url() ?>assets/js/script.js"></script>-->
 
 <!-- Datatables -->
-<script src="<?php echo  base_url() ?>assets/datatables/DataTables-1.12.0/js/jquery.dataTables.min.js"></script>
+<!--<script src="<?php echo  base_url() ?>assets/datatables/DataTables-1.12.0/js/jquery.dataTables.min.js"></script>
 <script src="<?php echo  base_url() ?>assets/datatables/Buttons-2.2.3/js/dataTables.buttons.min.js"></script>
 <script src="<?php echo  base_url() ?>assets/datatables/JSZip-2.5.0/jszip.min.js"></script>
 <script src="<?php echo  base_url() ?>assets/datatables/pdfmake-0.1.36/pdfmake.min.js"></script>
 <script src="<?php echo  base_url() ?>assets/datatables/pdfmake-0.1.36/vfs_fonts.js"></script>
 <script src="<?php echo  base_url() ?>assets/datatables/Buttons-2.2.3/js/buttons.html5.min.js"></script>
-<script src="<?php echo  base_url() ?>assets/datatables/Buttons-2.2.3/js/buttons.print.min.js"></script>
+<script src="<?php echo  base_url() ?>assets/datatables/Buttons-2.2.3/js/buttons.print.min.js"></script>-->
 
 <script>
     $(document).ready(function() {
