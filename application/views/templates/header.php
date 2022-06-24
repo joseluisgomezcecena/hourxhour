0cdd3fc6a75bab83836adf7b33fa9fea2c2fa762
Magui Campos <88505411+MaguiCampos@users.noreply.github.com>
2022-06-24 16:07:56 -0700

    comment head

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -212,10 +212,10 @@
                         Load Plan
                     </a>
                 </div>-->
-                <!--<a href="#no-link" class="active" data-toggle="collapse" data-target="#reports">
+                <a href="#no-link" class="active" data-toggle="collapse" data-target="#reports">
                     <span class="collapse-indicator la la-arrow-circle-down"></span>
                     Reports
-                </a>-->
+                </a>
                 <div id="reports" class="collapse">
                     <a href="<?php echo base_url(); ?>index.php/daily_report">
                         Daily Report
@@ -224,10 +224,10 @@
                         Custom Report
                     </a>
                 </div>
-                <a href="#no-link" class="active" data-toggle="collapse" data-target="#import_reports">
+                <!--<a href="#no-link" class="active" data-toggle="collapse" data-target="#import_reports">
                     <span class="collapse-indicator la la-arrow-circle-down"></span>
                     Import Reports
-                </a>
+                </a>-->
                 <div id="import_reports" class="collapse">
                     <a href="<?php echo base_url(); ?>import_report/tempus">
                         Tempus Report
